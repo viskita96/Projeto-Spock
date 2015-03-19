ae89120c122694331a7423f2e8ee78b800d48fee
viskita96 <viskita96@gmail.com>
2015-03-19 15:08:17 -0300

    Update Joguinho spock.py

--- Joguinho spock.py
+++ Joguinho spock.py
@@ -7,10 +7,10 @@ Created on Wed Mar 18 18:05:42 2015
 import random
 list = ["pedra","papel","spock","lagarto","tesoura"]
 
-pcs=0
-pls=0
 play = True
 while play == True:
+    pcs=0
+    pls=0
     play = False
     while pcs<3 and pls<3:
         pc = random.choice(list)
@@ -20,35 +20,34 @@ while play == True:
         if pc=="pedra" and (player=="tesoura" or "lagarto"):
             pcs+=1
             print('Computador ganhou a rodada \n Computador ', pcs, ', Jogador ' ,pls)
-        elif player=="pedra" and (pc=="tesoura" or "lagarto"):
+        elif player=="pedra" and (pc=="tesoura" or pc=="lagarto"):
             pls+=1
             print('Você ganhou a rodada \n Computador ', pcs ,', Jogador ' ,pls)
-        elif pc=="papel" and (player=="pedra" or "spock"):
+        elif pc=="papel" and (player=="pedra" or pc=="spock"):
             pcs+=1
             print('Computador ganhou a rodada \n Computador ', pcs, ', Jogador ' ,pls)
-        elif player=="papel"and (pc=="pedra" or "spock"):
+        elif player=="papel"and (pc=="pedra" or pc=="spock"):
             pls+=1
             print('Você ganhou a rodada \n Computador ', pcs ,', Jogador ' ,pls)
-        elif pc=="spock" and (player=="pedra" or "tesoura"):
+        elif pc=="spock" and (player=="pedra" or pc=="tesoura"):
             pcs+=1
             print('Computador ganhou a rodada \n Computador ', pcs, ', Jogador ' ,pls)
-        elif player=="spock" and (pc=="pedra" or "tesoura"):
+        elif player=="spock" and (pc=="pedra" or pc=="tesoura"):
             pls+=1
             print('Você ganhou a rodada \n Computador ', pcs ,', Jogador ' ,pls)
-        elif pc=="lagarto" and (player=="papel" or "spock"):
+        elif pc=="lagarto" and (player=="papel" or pc=="spock"):
             pcs+=1
             print('Computador ganhou a rodada \n Computador ', pcs, ', Jogador ' ,pls)
-        elif player=="lagarto" and (pc=="papel" or "spock"):
+        elif player=="lagarto" and (pc=="papel" or pc=="spock"):
             pls+=1
             print('Você ganhou a rodada \n Computador ', pcs ,', Jogador ' ,pls)
-        elif pc=="tesoura" and (player=="papel" or "lagarto"):
+        elif pc=="tesoura" and (player=="papel" or pc=="lagarto"):
             pcs+=1
             print('Computador ganhou a rodada \n Computador ', pcs, ', Jogador ' ,pls)
-        elif player=="tesoura" and (player=="papel" or "lagarto"):
+        elif player=="tesoura" and (player=="papel" or pc=="lagarto"):
             pls+=1
             print('Você ganhou a rodada \n Computador ', pcs ,', Jogador ' ,pls)
         elif player == pc:
-            print('Empate \n Computador ', pcs ,', Jogador ' ,pls)
+            print('Deu empate, jogue novamente \n Computador ', pcs ,', Jogador ' ,pls)
         else:
             print('Voce escreveu algo errado, digite novamente')
-        
